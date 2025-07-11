f7e47c84d54487c003b07a076c9544865a2cd2df
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-07-11 15:36:24 +0200

    Se han hecho todos los ejercicios, falta ajustarlos en consola

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-// Func Suma
-const num1 = parseInt(prompt("Ingrese el primer número"));
-const num2 = parseInt(prompt("Ingrese el segundo número"));
+// Level 1
+// Exercise 1
+const num1 = 3;
+const num2 = 5;
 
 function suma(num1, num2){
     return num1 + num2
 }
 document.getElementById("suma").innerHTML = suma(num1, num2);
 
-// Func arrow with random number
+// Exercise 2
 const randomNumber = () => Math.floor(Math.random() * 101);
 document.getElementById("random-number").innerHTML = randomNumber();
 
-// Clase with arrow func
-persona = new Persona (prompt("Ingrese su nombre"));
+// Exercise 3
+persona = new Persona ("Edgar");
 
 document.getElementById("saludar").innerHTML = persona.toString();
+
+// Level 2
+// Exercise 4
+const numbers = [1, 2, 3, 4, 5];
+const printNumbers = () => {
+    for (let number of numbers){
+        console.log(number);
+    }
+}
+printNumbers();
+
+// Exercise 5
+setTimeout(() => {
+  console.log("Este mensaje aparece 3 segundos despúes de cargar el DOM.");
+}, "3000");
