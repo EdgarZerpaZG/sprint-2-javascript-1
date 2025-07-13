99b121c4ccf99a194c6f9bc7e14df783fc2ec547
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-07-13 19:42:04 +0200

    Ejercicios rest y spread

--- js/main.js
+++ js/main.js
@@ -1,58 +1,25 @@
 // Level 1
 // Exercise 1
-console.log("1. Callback básico: ");
-const name1 = "Edgar";
-function procesar(name1, callback){
-    console.log(name1);
-    callback(name1);
-}
-procesar(name1, function(name1){
-    console.log("Saludos de " + name1 + " desde el callback");
-});
+console.log("1. Spread operator en arrays: ");
+
 
 // Exercise 2
-console.log("2. Callback con operaciones matemáticas: ");
-const num1 = "Edgar";
-const num2 = "Zerpa";
-function calculadora(num1, num2, calc){
-    calc(num1, num2);
-}
-calculadora(num1, num2, function(num1, num2){
-    console.log("Mi nombre es " + num1 + " " + num2);
-})
+console.log("2. Rest operator en funciones: ");
+
 
 // Level 2
 // Exercise 3
-console.log("3. Callback en funciones asíncronas: ");
-const name2 = "Edgar";
-function esperarSaludo(name2, esperar){
-    esperar(name2);
-}
-esperarSaludo(name2, function(name2){
-    setTimeout(() => {
-        console.log("Soy " + name2 +  " y envío un saludo!");
-    }, 2000);
-});
+console.log("3. Copiar objetos con spread operator: ");
+
 
 // Exercise 4
-console.log("4. Callback con array: ");
-const names = ["Edgar", "Carlos", "Luis"];
-function procesarElementos(names, elements){
-    elements(names);
-}
-procesarElementos(names, function(names){
-    names.forEach(element => {
-        console.log(element)
-    });
-});
+console.log("4. Rest operator en destructuring: ");
+
 
 // Level 3
 // Exercise 5
-console.log("5. Callback transformar a mayúscula: ");
-const sentencia = "Hola mundo!";
-function procesarCadena(sentencia, cadena){
-    cadena(sentencia);
-}
-procesarCadena(sentencia, function(sentencia){
-    console.log(sentencia.toUpperCase());
-})
+console.log("5. Spread operator en funciones: ");
+
+
+// Exercise 6
+console.log("6. Fusionar objetos con spread operator: ");
