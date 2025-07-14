123b4f6c43280d992d053421abe37529db4546c4
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-07-14 13:55:58 +0200

    Ejercicios realizados

--- js/main.js
+++ js/main.js
@@ -1,53 +1,48 @@
 // Level 1
 // Exercise 1
-console.log("1. Map: ");
-const array1 = [1, 2, 3, 4];
-const arrayNew1 = array1.map(function(elements){
-    return elements ** 2;
-});
-console.log(arrayNew1);
+console.log("1. forEach: ");
+const noms1 = ['Anna', 'Bernat', 'Clara'];
+noms1.forEach((element) => console.log(element));
 
 // Exercise 2
-console.log("2. Filter: ");
-const array2 = [1, 2, 3, 4];
-const arrayNew2 = array2.filter((arrayMini2) => arrayMini2 % 2 == 0);
-console.log(arrayNew2);
+console.log("2. for-of: ");
+const noms2 = ['Anna', 'Bernat', 'Clara'];
+for(nom2 of noms2){
+    console.log(nom2);
+}
 
 // Exercise 3
-console.log("3. Find: ");
-const array3 = [1, 10 , 8, 11];
-const arrayNew3 = array3.find((arrayMini3) => arrayMini3 >= 10);
-console.log(arrayNew3);
-
-// Exercise 4
-console.log("4. Reduce: ");
-const array4 = [13, 7, 8, 21];
-const valorInicial = 0;
-const total = array4.reduce((acumulador, actualValor) => acumulador + actualValor, valorInicial);
-console.log(total);
+console.log("3. filter: ");
+const noms3 = [1, 2, 3, 4, 5, 6];
+const noms3Par = noms3.filter((noms) => noms % 2 == 0);
+console.log(noms3Par);
 
 // Level 2
-// Exercise 5
-console.log("5. Funcion con todo lo anterior: ");
-const array5 = [ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ];
-const arrays = (array5) => {
-    let arrayNew5 = array5.filter((arrayMini5) => arrayMini5 >= 10);
-    console.log(arrayNew5);
-    let arrayNewNew5 = arrayNew5.map((arrayNewMini5) => arrayNewMini5 * 2);
-    console.log(arrayNewNew5);
-    let arrayActualInicial = 0;
-    let arrayNewNewNew5 = arrayNewNew5.reduce((arrayAcumulador, arrayActualValor) => arrayAcumulador + arrayActualValor, arrayActualInicial)
-    console.log(arrayNewNewNew5);
-    console.log(arrayNewNewNew5 * 2);
-    return arrayNew5, arrayNewNew5, arrayNewNewNew5;
+// Exercise 4
+console.log("4. for-in: ");
+const obj = { 
+    nom: "Ona", 
+    edat: 25, 
+    ciutat: 'Barcelona'
+};
+for(const data in obj){
+    console.log(`${data}: ${obj[data]}`);
 }
-arrays(array5);
+
+// Exercise 5
+console.log("5. for-of with break: ");
+const noms4 = [1, 2, 3, 4, 5, 6];
+for(const nom4 of noms4){
+    if(nom4 == 5){
+        break;
+    }
+    console.log(nom4)
+}
 
 // Level 3
 // Exercise 6
-console.log("6. Every/Some: ");
-const array6 = [11, 12, 13, 14];
-const arrayEvery6 = array6.every((arrayMini6) => arrayMini6 > 10);
-const arraySome6 = array6.every((arrayMini6) => arrayMini6 > 10);
-console.log(arrayEvery6);
-console.log(arraySome6);
+console.log("6. For-of index: ");
+const noms5 = ['Anna', 'Bernat', 'Clara'];
+for(const nom5 of noms5){
+    console.log("El nombre " + nom5 + " está en la posición " + noms5.indexOf(nom5));
+}
