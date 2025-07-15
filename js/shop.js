b633d41560ff49242b524dca5d77a797c742b158
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-07-15 13:47:15 +0200

    Estructura inicial de la tienda creada

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,42 @@
+// Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 const cart = [];
 
 const total = 0;
+
+// Exercise 1
+const buy = (id) => {
+    // 1. Loop for to the array products to get the item to add to cart
+    // 2. Add found product to the cart array
+}
+
+// Exercise 2
+const cleanCart = () =>  {
+
+}
+
+// Exercise 3
+const calculateTotal = () =>  {
+    // Calculate total price of the cart using the "cartList" array
+}
+
+// Exercise 4
+const applyPromotionsCart = () =>  {
+    // Apply promotions to each item in the array "cart"
+}
+
+// Exercise 5
+const printCart = () => {
+    // Fill the shopping cart modal manipulating the shopping cart dom
+}
+
+
+// ** Nivell II **
+
+// Exercise 7
+const removeFromCart = (id) => {
+
+}
+
+const open_modal = () =>  {
+    printCart();
+}
