8a68dfaa19fd5e3046b7632276e3dcd76537fc6b
EdgarZerpaZG <edgarzerpa83@gmail.com>
2025-07-16 13:40:44 +0200

    Ejercicio 1

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,82 @@
+console.log("Saludos desde shop.js")
+// Products
+// async function obtenerDatosJSON() {
+//   try {
+//     const respuesta = await fetch('../json/products.json');
+//     const datos = await respuesta.json();
+//     console.log(datos);
+//   } catch (error) {
+//     console.error('Error al obtener los datos:', error);
+//   }
+// }
+// obtenerDatosJSON();
+
+const products = [
+    {
+        id: 1,
+        name: 'cooking oil',
+        price: 10.5,
+        type: 'grocery',
+        offer: {
+            number: 3,
+            percent: 20
+        }
+    },
+    {
+        id: 2,
+        name: 'Pasta',
+        price: 6.25,
+        type: 'grocery'
+    },
+    {
+        id: 3,
+        name: 'Instant cupcake mixture',
+        price: 5,
+        type: 'grocery',
+        offer: {
+            number: 10,
+            percent: 30
+        }
+    },
+    {
+        id: 4,
+        name: 'All-in-one',
+        price: 260,
+        type: 'beauty'
+    },
+    {
+        id: 5,
+        name: 'Zero Make-up Kit',
+        price: 20.5,
+        type: 'beauty'
+    },
+    {
+        id: 6,
+        name: 'Lip Tints',
+        price: 12.75,
+        type: 'beauty'
+    },
+    {
+        id: 7,
+        name: 'Lawn Dress',
+        price: 15,
+        type: 'clothes'
+    },
+    {
+        id: 8,
+        name: 'Lawn-Chiffon Combo',
+        price: 19.99,
+        type: 'clothes'
+    },
+    {
+        id: 9,
+        name: 'Toddler Frock',
+        price: 9.99,
+        type: 'clothes'
+    }
+]
+// console.log(products)
+
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 const cart = [];
 
@@ -7,6 +86,11 @@ const total = 0;
 const buy = (id) => {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array
+        const item = products.filter(product => product.id === id)
+        console.log(item[id - 1])
+        cart.push([item[id - 1]]);
+        console.log(cart);
+
 }
 
 // Exercise 2
